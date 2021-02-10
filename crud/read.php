238856c2ca18ec2f464113a3f72f1fc9efdf4077
Samu <73467682+Samu@users.noreply.github.com>
2021-02-10 08:23:26 +0200

    Update read.php

--- crud/read.php
+++ crud/read.php
@@ -1,26 +1,37 @@
 <?php
-include 'db.php'; 
-$sql = "select * from studentsinfo WHERE (fname != '' AND lname != '' AND city != '' AND groupid != '')";
-$result = $conn->query($sql);
-if($result ->num_rows > 0) {
-//fetch_assoc(): It fetches result as an associative array.
-echo "<table border=\"1\"><tr><th>ID</th><th>First Name</th><th>Last Name</th>
-<th>City</th><th>Group ID</th><th>Created at</th> </tr>";
-    while($row = $result ->fetch_assoc()){
-            echo "<tr><td>" . $row["id"] . "</td><td>". $row["fname"]."</td> <td>". $row["lname"] 
+    include 'db.php'; 
+    $sql = "select * from studentsinfo WHERE (fname != '' AND lname != '' AND city != '' AND groupid != '')";
+    $result = $conn->query($sql);
+    if($result ->num_rows > 0) {
+    //fetch_assoc(): It fetches result as an associative array.
+    echo "<table border=\"1\">
+            <tr>
+                <th>ID</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>City</th>
+                <th>Group ID</th>
+                <th>Created at</th> 
+            </tr>";
             
-            ."</td><td>". $row["city"]. "</td><td>". $row["groupid"]
-            ."</td> <td>" .$row["timestamp"] . "</td></tr>";
+        while($row = $result ->fetch_assoc()){
+                echo "<tr>
+                        <td>". $row["id"] . "</td>
+                        <td>". $row["fname"]."</td> 
+                        <td>". $row["lname"]. "</td>
+                        <td>". $row["city"]. "</td>
+                        <td>". $row["groupid"]."</td> 
+                        <td>". $row["timestamp"]. "</td>
+                    </tr>";
+        }
+        echo "</table>";
     }
-    echo "</table>";
-}
-// You can type different sql queries based on your needs
-// The output as of now does not look good. Your task is to format it properly with HTML tables. 
-else 
-{
-    echo "no results";
-}
+    // You can type different sql queries based on your needs
+    // The output as of now does not look good. Your task is to format it properly with HTML tables. 
+    else 
+    {
+        echo "no results";
+    }
 
-
-$conn->close();
+    $conn->close();
 ?>
