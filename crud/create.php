e02146e62f23e210cb2a102149345e3e04f58d44
Samu <73467682+Samu@users.noreply.github.com>
2021-02-10 08:23:41 +0200

    Update create.php

--- crud/create.php
+++ crud/create.php
@@ -1,20 +1,22 @@
-<?php include 'db.php' ;
-$fname= $_POST['fname'];
-$lname= $_POST['lname'];
-$city= $_POST['city'];
-$groupid= $_POST['groupid'];
-$sql="insert into studentsinfo (fname, lname, city, groupid)
-values('$fname', '$lname', '$city', '$groupid')";
+<?php 
+    include 'db.php';
 
-if($conn->query($sql) === TRUE) {
-    echo "New record added";
-    echo "<a href='update.php' class='top'>Home </a>";
-}
-else
-{
-    echo "ERROR: " .$sql. "<br>" . $conn->error;
-}
-$conn->close();
+    $fname= $_POST['fname'];
+    $lname= $_POST['lname'];
+    $city= $_POST['city'];
+    $groupid= $_POST['groupid'];
+    $sql="insert into studentsinfo (fname, lname, city, groupid)
+    values('$fname', '$lname', '$city', '$groupid')";
+
+    if($conn->query($sql) === TRUE) {
+        echo "New record added";
+        echo "<a href='update.php' class='top'>Home </a>";
+    }
+    else
+    {
+        echo "ERROR: " .$sql. "<br>" . $conn->error;
+    }
+    $conn->close();
 
 
 #I didi some change
