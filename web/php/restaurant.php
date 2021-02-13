ef34cca52bef1c7c3b23110d02f6e5b80e4f2e73
Samu <73467682+Samu@users.noreply.github.com>
2021-02-13 17:30:46 +0200

    Create restaurant.php

---
+++ web/php/restaurant.php
@@ -0,0 +1,57 @@
+
+<!-- Include header from header.php file -->
+<?php include 'header.php'; ?>
+
+
+<!-- Restaurant page content -->
+
+ <!-- Header -->
+ <div class="row">
+              <div class="col-md-12">
+                <h1>Hame Hotel Restaurant</h1>
+              </div>
+          </div>
+
+
+          <!-- Page Content-->
+          <div class="content">
+              <div class="row">
+                <div class="col-md-6">
+                    <img src="images/morning_coffee.jpg" class="img-fluid" alt="Poaring coffee in Hame Hotel Restaurant" width="1350" height="900">
+                </div>
+                <div class="col-md-6"> 
+                    <article>
+                      <h2>Breakfest</h2>
+                      <p>Our delicious breakfest every day:
+                        <ul>
+                            <li>6:00 - 12:00</li>
+                        </ul>
+                      </p>
+                    </article>
+                </div>
+              </div>
+          </div>
+
+          <div class="content">
+              <div class="row">
+                <div class="col-md-6">
+                  <article>
+                      <h2>Dinner</h2>
+                      <p>Restaurant open everyday:
+                        <ul>
+                            <li>12:00 - 22:00</li>
+                        </ul>
+                      </p>
+                  </article>   
+                </div>
+                <div class="col-md-6">
+                  <img src="images/dinner.jpg" class="img-fluid" alt="dinner picture with delicious food from Hame Hotel Restaurant" width="1350" height="900">
+                </div>
+              </div>
+          </div>
+
+
+
+
+<!-- Include footer from footer.php file -->
+<?php include 'Footer.php'; ?>
