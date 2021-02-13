a703a6dfb24868b6966e0649571c5fa535068a46
Samu <73467682+Samu@users.noreply.github.com>
2021-02-13 17:30:44 +0200

    Create parking.php

---
+++ web/php/parking.php
@@ -0,0 +1,38 @@
+
+<!-- Include header from header.php file -->
+<?php include 'header.php'; ?>
+
+<!-- Parking page content -->
+
+
+            <!-- Header -->
+            <div class="row">
+                <div class="col-md-12">
+                    <header><h1>Hame Hotel - Parking</h1></header>
+                </div>
+            </div>
+
+
+            <!-- Page Content-->
+            <main>
+                <div class="content">
+                    <div class="row">
+                        <!-- Left side article 1 -->
+                        <div class="col-md-12">
+                            <article>
+                                <h2>We offer safe parking for your vehicle</h2>
+                                <p>
+                                  Hame Hotel offers 24/7 guarded parking lots for customers. Please read more about how to drive to our Hotel from Contact page.
+                                </p>
+                                <a href="contact_hossein.html" class="btn btn-primary">Contact >></a>
+                            </article>
+                        </div>
+                    </div>
+                </div>
+
+            </main>
+
+
+<!-- Include footer from footer.php file -->
+<?php include 'Footer.php'; ?>
+
