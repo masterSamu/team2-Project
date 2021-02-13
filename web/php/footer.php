33c4e8dcd97c14bdb6470cc99c2a378218b9ee95
Samu <73467682+Samu@users.noreply.github.com>
2021-02-13 17:28:49 +0200

    Create footer.php

---
+++ web/php/footer.php
@@ -0,0 +1,24 @@
+
+<!-- Footer file for Hame Hotel website -->
+
+            <!-- Footer -->
+            <div class="row">
+                <div class="col-md-12">
+                    <footer>
+                        <address>
+                            Phone number: +358401234567<br>
+                            Address: 28 Visamäentie, Hämeenlinna,<br>
+                            Country: Finland
+                        </address>
+                    </footer>
+                </div>
+            </div>
+
+        </div> <!-- End of container-fluid -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+
+    </body>
+</html>
