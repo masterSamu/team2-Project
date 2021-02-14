3feae4c23ce921b2dfa394c2eba3aed8d9de2f2d
Samu <73467682+Samu@users.noreply.github.com>
2021-02-14 16:39:22 +0200

    Create meetings.php

---
+++ web/php/meetings.php
@@ -0,0 +1,175 @@
+<!doctype html>
+<html lang="en">
+
+  <head>
+
+      <!-- Required meta tags -->
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="keywords" content="Hotel parking, Hämeenlinna">
+      <meta name="description" content="Hotel parking in Hämeenlinna">
+      <meta name="author" content="team2">
+
+      <!-- Styling files -->
+      <?php include 'styling-links.php'; ?>
+
+      
+      <title>Hame Hotel - Meetings</title>
+
+  </head>
+
+
+<!-- Include navigation bar from navbar.php file -->
+<?php include 'navbar.php'; ?>
+
+    <!-- Header -->
+    <div class="row">
+        <div class="col-md-12">
+            <h1>World class business meetings at Hame Hotel</h1>
+        </div>
+    </div>
+
+<main>
+
+    <div class="content">
+      <div class="row">
+        <div class="col-md-6">
+          <img src="images/hame-hotel-meetingroom-main.jpg" class="img-fluid" alt="double bed room Photo" width="1350" height="900">
+        </div>
+        <div class="col-md-6">
+            <h2>Versatile meeting rooms combined with excellent service and great value for money</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac nisi ultrices erat pretium tincidunt ac a est. Suspendisse sagittis vehicula vestibulum. Morbi sed rutrum arcu. Etiam pretium lacus at lectus convallis ullamcorper. Aliquam sollicitudin, enim id tempor sodales, ligula tortor tristique felis, a luctus neque dolor nec turpis. Mauris erat lorem, egestas a ex sed, consequat venenatis elit. Etiam dictum augue ut neque consectetur, id dictum leo suscipit. Nullam bibendum quam ut scelerisque varius. Integer iaculis pretium libero ac dignissim. Quisque id tortor vel nisl egestas euismod.</p>
+            <p>Donec tristique nunc a eros sollicitudin ultrices. Maecenas convallis, dolor a pretium efficitur, lorem arcu facilisis nibh, a auctor libero dui ut ipsum. Vivamus mollis ullamcorper felis, ac scelerisque elit volutpat eget. Mauris tincidunt ipsum ac est dignissim convallis. Etiam auctor, velit ut porta cursus, leo neque sollicitudin turpis, nec varius ipsum nisi ultricies arcu. Maecenas tincidunt risus ut mollis aliquam. Vivamus eu elit vehicula, tempor mauris eu, rutrum ante. Fusce velit enim, finibus non odio at, rutrum egestas est. Vestibulum sodales massa eu sem suscipit, a sagittis erat varius. Sed gravida massa tellus, eget ultrices est congue et. Aliquam finibus ornare elit. Integer aliquam aliquam dolor, vel cursus ipsum. Mauris ut libero nec arcu malesuada imperdiet. Curabitur non metus et elit efficitur vestibulum eget vel enim. Sed et mi finibus, faucibus orci et, gravida nisl. Aenean tristique convallis lorem, sed dapibus ipsum molestie et.</p>
+        </div>
+      </div>  
+    </div>    
+    
+    <div class="content">
+      <p class="menu-describtion">
+        <h2 style="text-align: center;">Meet the best meeting Facilities in Hame Hotel</h2>
+      </p>
+      
+      <div class="flex-property-usage"><!--using flexbox -->
+
+          <div class="card h-100 mx-1 my-1" style="max-width: 550">
+            <img src="images/hame-hotel-meetingroom1-photo.jpg" class="card-img-top" alt="Small Meeting room suitable for under 20 person meetings image" widht="500" height="350" >
+            <div class="card-body">
+              <h5 class="card-title">5-20 persons - 68m2</h5>
+              <p><br></p>
+              <h4 class="card-text">Small Meeting Room</h4>
+              <p>Small Meeting room suitable for under 20 person meetings</p>
+              <a href="#" class="btn btn-primary">Read more >></a>
+            </div>
+          </div>
+        
+          <div class="card h-100 mx-1 my-1" style="max-width: 550">
+            <img src="images/hame-hotel-meetingroom2-photo.jpg" class="card-img-top" alt="Medium Meeting room suitable for under 40 person meetings image" widht="500" height="350" >
+            <div class="card-body">
+              <h5 class="card-title">10-40 persons - 85m2</h5>
+              <p><br></p>
+              <h4 class="card-text">Medium Meeting room</h4>
+              <p>Medium Meeting room suitable for under 40 person meetings</p>
+              <a href="#" class="btn btn-primary">Read more >></a>
+            </div>
+          </div>
+        
+          <div class="card h-100 mx-1 my-1" style="max-width: 550">
+            <img src="images/hame-hotel-meetingroom3-photo.jpg" class="card-img-top" alt="Conference room is big salone with up to 300 person Sessions" widht="500" height="350" >
+            <div class="card-body">
+              <h5 class="card-title">10-300 persons - 350m2</h5>
+              <p><br></p>
+              <h4 class="card-text">Conference Room</h4>
+              <p>Conference room is big salone with up to 300 person Sessions</p>
+              <a href="#" class="btn btn-primary">Read more >></a>
+            </div>
+          </div>
+
+        </div><!-- end offlexbox -->
+    </div>
+      
+    <div class="content">
+      <div class="row">
+        <div class="col-md-12">
+          <h2>Meetings in Hameenlinna<br>
+              Versatile conference facilities in the city centre</h2>
+          <div class="describe">
+            <p><strong>Hame Hotel's unique conference facilities provide a modern setting for meetings from 5 up to 300 people
+            </strong></p>
+            <p><b>Why arrange a meeting at Hame Hotel?</b> Hame Hotel offers pleasant, efficient and versatile meeting, course, seminar or training rooms in the heart of Hameenlinna, 200 metres from the Central Railway Station. Altogether, the conference rooms have space for over 450 people. Professional and friendly staff will help with all the meeting arrangements. With an atmosphere of over 100 years, the Art Nouveau-style conference annex is unique. Crystal chandeliers, antique furniture, stained glass windows, bay windows, portraits and paintings on the walls, upright pianos, and grand pianos create a historical atmosphere like no other. Versatile meeting breakfast from the coffee buffet, a meeting lunch at the restaurant’s buffet, and afternoon coffee with sweet pastries and fruit are included in the full day meeting package.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="content" >
+        <div class="col-md-12">
+          <h3 class="review-title" style="text-align: center;"><b>What our customers say</b></h3>
+
+          <!-- start of Carousel-->
+              <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                  <div class="carousel-inner" id="meeting-page-carousel">
+                    <div class="carousel-item active">
+                        <div class="carousel-review">
+                          <article>
+                            <p>
+                              "Having one single person as a permanent contact makes for a very effective working relationship.
+                              This person knows our company very well. He links up with the local Atradius collectors,
+                              who have local expertise and know the procedures and regulations in the countries where they work.
+                              Atradius Collections reports straightforward and totally transparent. Plus its reporting is adapted to our financial structure.
+                              Atradius Collections also know our customers well, so very often they can operate proactively – and they always act quickly."
+                            </p>
+                            <p>Stefan Craciun</p>
+                            <p>Senior Manager Credit and Collection Colt Technology Services</p>        
+                          </article>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="carousel-review">
+                          <article>
+                            <p>
+                              "Having one single person as a permanent contact makes for a very effective working relationship.
+                              This person knows our company very well. He links up with the local Atradius collectors,
+                              who have local expertise and know the procedures and regulations in the countries where they work.
+                              Atradius Collections reports straightforward and totally transparent. Plus its reporting is adapted to our financial structure.
+                              Atradius Collections also know our customers well, so very often they can operate proactively – and they always act quickly."
+                            </p>
+                            <p>Stefan Craciun</p>
+                            <p>Senior Manager Credit and Collection Colt Technology Services</p>        
+                          </article>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="carousel-review">
+                          <article>
+                            <p>
+                              "Having one single person as a permanent contact makes for a very effective working relationship.
+                              This person knows our company very well. He links up with the local Atradius collectors,
+                              who have local expertise and know the procedures and regulations in the countries where they work.
+                              Atradius Collections reports straightforward and totally transparent. Plus its reporting is adapted to our financial structure.
+                              Atradius Collections also know our customers well, so very often they can operate proactively – and they always act quickly."
+                            </p>
+                            <p>Stefan Craciun</p>
+                            <p>Senior Manager Credit and Collection Colt Technology Services</p>        
+                          </article>
+                        </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                    </a>
+                </div> 
+            </div>
+        </div>
+      </div>
+    </div>
+  </main>
+
+
+  <!-- Footer from footer.php file -->
+ <?php include 'footer.php'; ?>
+
+
