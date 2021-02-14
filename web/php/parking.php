f7d7c65c09043ebcf33821fb75a38f610f3b5f5a
Samu <73467682+Samu@users.noreply.github.com>
2021-02-14 16:39:28 +0200

    Update parking.php

--- web/php/parking.php
+++ web/php/parking.php
@@ -1,14 +1,34 @@
+<!doctype html>
+<html lang="en">
 
-<!-- Include header from header.php file -->
-<?php include 'header.php'; ?>
+  <head>
+
+      <!-- Required meta tags -->
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="keywords" content="Hotel parking, Hämeenlinna">
+      <meta name="description" content="Hotel parking in Hämeenlinna">
+      <meta name="author" content="team2">
+
+      <!-- Styling files -->
+      <?php include 'styling-links.php'; ?>
+
+      
+      <title>Hame Hotel - Parking</title>
+
+  </head>
+
+
+<!-- Include navigation bar from navbar.php file -->
+<?php include 'navbar.php'; ?>
 
 <!-- Parking page content -->
 
 
-            <!-- Header -->
+            <!-- Heading -->
             <div class="row">
                 <div class="col-md-12">
-                    <header><h1>Hame Hotel - Parking</h1></header>
+                    <h1>Hame Hotel Parking</h1>
                 </div>
             </div>
 
@@ -24,7 +44,7 @@
                                 <p>
                                   Hame Hotel offers 24/7 guarded parking lots for customers. Please read more about how to drive to our Hotel from Contact page.
                                 </p>
-                                <a href="contact_hossein.html" class="btn btn-primary">Contact >></a>
+                                <a href="contact.php" class="btn btn-primary">Contact >></a>
                             </article>
                         </div>
                     </div>
