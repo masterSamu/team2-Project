9605f07b429cacad63c3afa29bd0cff560ed408b
Samu <73467682+Samu@users.noreply.github.com>
2021-02-23 11:00:40 +0200

    Update home.php

--- web/php/home.php
+++ web/php/home.php
@@ -5,6 +5,7 @@
   $metaKeywords = "Hotel, Hämeenlinna";
   $metaDescription = "Hotel, Hämeenlinne";
   $title = "Hame Hotel - Home";
+  $scripts = '<script src="scripts/validateSubscription.js"></script>';
 
   include 'header.php';
 ?>
@@ -135,39 +136,7 @@
                           <!-- Form error message -->
                           <div id="error"></div>
                         </div>
-                        <script>
-                        // This function will give notifications to user when filling subscribe-newsletter form
-                        function validateSubscribeForm() {
-                          let email = document.forms["subscribe-newsletter"]["email"];
-                          let checkBox = document.getElementById('checkbox');
-
-                          // check if email field is not empty.
-                          if (email.value == "") {
-                              event.preventDefault();
-                              document.getElementById("error").style.color = "red";
-                              document.getElementById("error").innerHTML = "Email field is empty";
-                              document.getElementById("email").style.borderType = "5px double";
-                              document.getElementById("email").style.borderColor = "red";
-                              return false;
-                            // check if checkbox is checked
-                          } else if (!checkBox.checked) {
-                              if (email.value != "") {
-                                document.getElementById("email").style.borderColor = "green";
-                              }
-                              event.preventDefault();
-                              document.getElementById("error").style.color ="red";
-                              document.getElementById("error").innerHTML = "You have not agreed to subscribe newsletter";
-                              document.getElementById("lblCheckbox").style.color = "red";
-                              document.getElementById("lblCheckbox").style.borderType ="5px double";
-                              return false;
-                          } else {
-                              document.getElementById("error").style.color = "red";
-                              document.getElementById("email").style.borderColor = "white";
-                              alert("You have succesfully subscribed our newsletter!");
-                              return true;
-                          }
-                        }
-                        </script>
+                        
                       </form>
                     </div>
                   </div>
