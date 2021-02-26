a2f3707492e4c1d0a9e77e33037ae2141ad377f6
Samu <73467682+Samu@users.noreply.github.com>
2021-02-26 20:15:04 +0200

    Update subscribe-newsletter.php
    clearing the code little bit

--- web/php/subscribe-newsletter.php
+++ web/php/subscribe-newsletter.php
@@ -1,7 +1,8 @@
 <?php
     include 'database.php';
 
-    $email = $_POST['email'];
+    // Change email field to lowercase letters before sending to database
+    $email = strtolower($_POST['email']);
     
     if (empty($email)) {
         echo "Email field is empty";
@@ -12,7 +13,17 @@
             // When query is submitted, move to homepage
             header('Location: home.php');
         } else {
-            include 'retrieve-emails.php';
+            // Check if email already exist
+            $sql = "SELECT email FROM subscriptions WHERE email='$email'";
+            $result = $conn->query($sql);
+            if($result ->num_rows > 0) {
+                $emailExists = true;
+            } else 
+            {
+                $emailExists = false;
+            }
+
+            // actions if email already exist.
             if ($emailExists == true) {
                 echo 'This email has already subscribed our newsletter';
                 echo '<br><button><a href="home.php">Get back to home page</a></button>';
@@ -21,10 +32,10 @@
             }
             
         }
+
+        // Free result set
+        mysqli_free_result($result);
+
         $conn->close();
     }
-
-
-
-
 ?>
