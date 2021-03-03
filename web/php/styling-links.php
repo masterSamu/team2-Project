9d353fe52c9eec05fec22122a7e334aefa482a0b
masterSamu <73467682+masterSamu@users.noreply.github.com>
2021-03-03 11:34:20 +0200

    Delete styling-links.php

--- web/php/styling-links.php
+++
@@ -1,9 +0,0 @@
-        <!--fonts-->
-        <link rel="preconnect" href="https://fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">    
-
-        <!-- Bootstrap CSS -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-        
-        <!-- Custom CSS -->
-        <link href="Default.css" rel="stylesheet">
